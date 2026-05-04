74495c25e443754e7e649224431e332b105eee4e
Antob0t <antolinmurcianavarro@gmail.com>
2026-05-04 16:41:43 +0200

    first commit

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,16 +1,2 @@
-/**
- * Import dependencies from node_modules
- * see commented examples below
- */
-
-// import 'some-node-module';
-// import SomeModule from 'some-node-module';
-
-/**
- * Write any other JavaScript below
- */
-
-+(function () {
-  const university = "UOC";
-  console.log(`Hello, ${university}!`);
-})();
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import * as bootstrap from 'bootstrap';
